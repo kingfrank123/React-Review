86ee3d2acd7949a554178cebba6ebc76371b532f
frank <frankyan@buffalo.edu>
2023-05-23 22:11:15 -0400

    todolist idea but static for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  const [newItem, setNewItem] = useState("")
+  const [todos, setToDos] = useState([])
+  
+  function handleSubmit(e){
+    e.preventDefault()
+    
+    setToDos((currentTodos) => {
+      return [
+        ...todos, 
+        {id : crypto.randomUUID(), 
+          title: newItem, 
+          completed: false}
+        ]
+    })
+  }
 
   return (
+    // fragment since react can only return one component
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit = {handleSubmit} className = "new-item-form">
+      {/* my form */}
+      <div className = "form-row">
+      <label htmlFor ="item"> New Item</label>
+      <input value={newItem} 
+        onChange={e => setNewItem(e.value)} 
+        type="text" 
+        id = "item"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className = "btn">Add</button>
+    </form>
+    <h1 className="header">To do List</h1>
+    <ul className="list"> 
+      <li>
+        <label>
+          <input type = "checkbox"/>
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type = "checkbox"/>
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      
+    </ul>
     </>
   )
 }
-
-export default App
